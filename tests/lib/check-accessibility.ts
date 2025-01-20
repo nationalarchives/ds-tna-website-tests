10a5425dfd7e9c3187e50a6a8d9d8d672211ef48
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-20 18:01:43 +0000

    Add TODO, remove test code

--- tests/lib/check-accessibility.ts
+++ tests/lib/check-accessibility.ts
@@ -4,28 +4,12 @@ import AxeBuilder from "@axe-core/playwright";
 const checkAccessibility: (page: Page) => void = async (page) => {
   await test.step("Check page accessibility", async () => {
     /* Ignore skip links and phase banner (for now) */
+    /* TODO: Remove ignore for phase banner once in header */
     const accessibilityScanResults = await new AxeBuilder({ page })
       .exclude(".tna-skip-link")
       .exclude(".tna-phase-banner")
       .analyze();
     const accessibilityScanViolations = accessibilityScanResults.violations;
-    // .map((violation) => {
-    //   let nodes = violation.nodes;
-    //   /* Ignore "Ensure all page content is contained by landmarks" for skip links and phase banners */
-    //   if (violation.id === "region") {
-    //     nodes = nodes.filter(
-    //       (node) =>
-    //         node.target.length === 1 &&
-    //         !node.target.includes(".tna-skip-link") &&
-    //         !node.target.includes(".tna-phase-banner"),
-    //     );
-    //     if (nodes.length === 0) {
-    //       return null;
-    //     }
-    //   }
-    //   return { ...violation, nodes };
-    // })
-    // .filter((violation) => violation);
     expect(accessibilityScanViolations).toEqual([]);
   });
 };
