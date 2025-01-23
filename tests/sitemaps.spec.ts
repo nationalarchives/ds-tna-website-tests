718deeb4c1997ea5c6a37ff2405dce642b4b55b0
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-23 18:19:20 +0000

    Add basic sitemaps tests

---
+++ tests/sitemaps.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test.fixme("/sitemaps/ redirect to XML sitemap", async ({ page }) => {
+  page.on("response", async (response) => {});
+  const response = await page.goto("/sitemaps/");
+  const contentType = await response?.headerValue("content-type");
+  expect(contentType).toEqual("application/xml; charset=utf-8");
+});
+
+test.fixme("main sitemap", async ({ page }) => {
+  page.on("response", async (response) => {
+    const contentType = await response.headerValue("content-type");
+    expect(contentType).toEqual("application/xml; charset=utf-8");
+  });
+  await page.goto("/sitemap.xml");
+});
+
+test.fixme("static pages sitemap", async ({ page }) => {
+  page.on("response", async (response) => {
+    const contentType = await response.headerValue("content-type");
+    expect(contentType).toEqual("application/xml; charset=utf-8");
+  });
+  await page.goto("/sitemaps/sitemap_1.xml");
+});
+
+test.fixme("first dynamic pages sitemap", async ({ page }) => {
+  page.on("response", async (response) => {
+    const contentType = await response.headerValue("content-type");
+    expect(contentType).toEqual("application/xml; charset=utf-8");
+  });
+  await page.goto("/sitemaps/sitemap_2.xml");
+});
+
+test.fixme("non-existant sitemap", async ({ page }) => {
+  const response = await page.goto("/sitemaps/sitemap_99999.xml");
+  const status = await response?.status();
+  expect(status).toEqual(404);
+});
