d2c34762ec707f607c1a1e3dc6d54f18dddb10a2
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-23 18:19:47 +0000

    Try a different method to test for redirects

--- tests/explore-the-collection/index.spec.ts
+++ tests/explore-the-collection/index.spec.ts
@@ -3,18 +3,34 @@ import validateHtml from "../lib/validate-html";
 import checkAccessibility from "../lib/check-accessibility";
 
 test("explore the collection without a trailing slash", async ({ page }) => {
-  page.on("response", (response) => {
-    expect(response.status()).toBeGreaterThanOrEqual(200);
-    expect(response.status()).toBeLessThan(400);
-    if (response.status() >= 300) {
-      response
-        .headerValue("location")
-        .then((location) =>
-          expect(location).toMatch(/\/explore-the-collection\/$/),
-        );
-    }
+  // page.on("response", (response) => {
+  //   expect(response.status()).toBeGreaterThanOrEqual(200);
+  //   expect(response.status()).toBeLessThan(400);
+  //   if (response.status() >= 300) {
+  //     response
+  //       .headerValue("location")
+  //       .then((location) =>
+  //         expect(location).toMatch(/\/explore-the-collection\/$/),
+  //       );
+  //   }
+  // });
+  // await page.goto("/explore-the-collection");
+  await page.route("**", async (route) => {
+    const response = await route.fetch({ maxRedirects: 0 });
+    let headers = response.headers();
+    expect(headers.location).toMatch(/\/explore-the-collection\/$/);
+    delete headers["location"];
+    delete headers["Location"];
+    return route.fulfill({
+      response: response,
+      headers: headers,
+    });
   });
-  await page.goto("/explore-the-collection");
+  const response = await page.goto("/explore-the-collection");
+  const status = await response?.status();
+  console.log(status);
+  expect(status).toBeGreaterThanOrEqual(300);
+  expect(status).toBeLessThan(400);
 });
 
 test("explore the collection home page", async ({ page }) => {
