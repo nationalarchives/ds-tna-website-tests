6a6e6a43c80481de34a81e2816b818f1914405fe
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-27 11:04:53 +0000

    Add tests for people pages

---
+++ tests/people/index.spec.ts
@@ -0,0 +1,15 @@
+import { test } from "@playwright/test";
+import validateHtml from "../lib/validate-html";
+import checkAccessibility from "../lib/check-accessibility";
+
+test("people index page", async ({ page }) => {
+  await page.goto("/people/");
+  await validateHtml(page);
+  await checkAccessibility(page);
+});
+
+test("person profile page", async ({ page }) => {
+  await page.goto("/people/vicky-iglikowski-broad/");
+  await validateHtml(page);
+  await checkAccessibility(page);
+});
