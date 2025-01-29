1381cbd2fdf38ddde8b93d1779ba2ad07e41fdc3
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-29 12:42:12 +0000

    Add dot reporting to CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI
     ? [
+        ["dot"],
         ["@estruyf/github-actions-reporter"],
         ["json", { outputFile: "test-results.json" }],
       ]
