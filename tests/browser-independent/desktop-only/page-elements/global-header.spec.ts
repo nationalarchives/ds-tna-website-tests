cf54139407059879db26efe71e6a92476ede55e7
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-02-10 15:56:05 +0000

    Fix header HTML snapshots

--- tests/browser-independent/desktop-only/page-elements/global-header.spec.ts
+++ tests/browser-independent/desktop-only/page-elements/global-header.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test(
   "global header has the correct markup with HTML only",
-  { tag: "@smoke" },
+  { tag: ["@smoke", "@wip"] },
   async ({ page }) => {
     await page.route("**/*", (route) => {
       return ["script", "stylesheet"].includes(route.request().resourceType())
@@ -10,8 +10,8 @@ test(
         : route.continue();
     });
     await page.goto("/explore-the-collection/"); // TODO: Change to homepage once updated
-    const header = await page.locator(".tna-global-header");
-    await expect(await header.innerHTML()).toMatchSnapshot();
+    const headerMain = await page.locator(".tna-global-header__main");
+    await expect(await headerMain.innerHTML()).toMatchSnapshot();
     // await expect(await header.screenshot()).toMatchSnapshot(
     //   "global-header-plain-html.png",
     // );
--- tests/browser-independent/page-elements/global-header.spec.ts
+++ tests/browser-independent/page-elements/global-header.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test(
   "global header has the correct markup after JS is added",
-  { tag: "@smoke" },
+  { tag: ["@smoke", "@wip"] },
   async ({ page }) => {
     await page.goto("/explore-the-collection/"); // TODO: Change to homepage once updated
-    const header = await page.locator(".tna-global-header");
-    await expect(await header.innerHTML()).toMatchSnapshot();
+    const headerMain = await page.locator(".tna-global-header__main");
+    await expect(await headerMain.innerHTML()).toMatchSnapshot();
   },
 );
