0295e4db6748d00f2ad38b8f9f3227f65ee3f1ff
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-04 12:45:04 +0000

    Update catalogue search tests

--- tests/catalogue/index.spec.ts
+++ tests/catalogue/index.spec.ts
@@ -23,38 +23,46 @@ test.beforeEach(async ({ context, baseURL }) => {
   ]);
 });
 
-test(
-  "search for records using the main search fields",
-  { tag: ["@wip", "@ui"] },
-  async ({ page }) => {
-    await page.goto("/catalogue/");
-    await expect(
-      page.getByRole("region", { name: "Cookies on The National Archives" }),
-    ).not.toBeVisible();
-    await expect(page.locator("h1")).toHaveText(/Welcome to the catalogue/);
+test("search for records", { tag: ["@wip", "@ui"] }, async ({ page }) => {
+  await page.goto("/catalogue/");
+  await expect(
+    page.getByRole("region", { name: "Cookies on The National Archives" }),
+  ).not.toBeVisible();
+  await expect(page.locator("h1")).toHaveText(/Welcome to the catalogue/);
 
-    await page.getByLabel("Search the catalogue").fill("plymouth");
-    await page.getByRole("button", { name: "Search" }).click();
-    await expect(page).toHaveURL(/[\?&]q=plymouth/);
-    await expect(page.getByRole("main")).toHaveText(
-      /Showing ([\d,]+) of ([\d,]+)/,
-    );
+  await page.getByLabel("Search the catalogue").fill("plymouth");
+  await page.getByRole("button", { name: "Search" }).click();
 
-    await page.getByLabel("Catalogue search results").fill("exeter");
-    await page.getByLabel("Catalogue search results").press("Enter");
-    await expect(page).toHaveURL(/[\?&]q=exeter/);
-    await expect(page.getByRole("main")).toHaveText(
-      /Showing ([\d,]+) of ([\d,]+)/,
-    );
+  await expect(page).toHaveURL(/[\?&]q=plymouth/);
+  await expect(page.getByRole("main")).toHaveText(
+    /Showing results ([\d,]+)–([\d,]+) of ([\d,]+)/,
+  );
 
-    await page.getByLabel("Catalogue search results").fill("");
-    await page.getByRole("button", { name: "Search" }).click();
-    await expect(page).toHaveURL(/[\?&]q=/);
-    await expect(page.getByRole("main")).toHaveText(
-      /Showing ([\d,]+) of ([\d,]+)/,
-    );
-  },
-);
+  await page.getByRole("link", { name: "Page 2" }).click();
+  await expect(page).toHaveURL(/[\?&]q=plymouth/);
+  await expect(page).toHaveURL(/[\?&]page=2/);
+
+  await page.getByRole("link", { name: "Next page" }).click();
+  await expect(page).toHaveURL(/[\?&]q=plymouth/);
+  await expect(page).toHaveURL(/[\?&]page=3/);
+
+  await page.getByLabel("Catalogue search results").fill("exeter");
+  await page.getByLabel("Catalogue search results").press("Enter");
+
+  await expect(page).toHaveURL(/[\?&]q=exeter/);
+  await expect(page).not.toHaveURL(/[\?&]page=2/);
+  await expect(page.getByRole("main")).toHaveText(
+    /Showing results ([\d,]+)–([\d,]+) of ([\d,]+)/,
+  );
+
+  await page.getByLabel("Catalogue search results").fill("");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await expect(page).toHaveURL(/[\?&]q=/);
+  await expect(page.getByRole("main")).toHaveText(
+    /Showing results ([\d,]+)–([\d,]+) of ([\d,]+)/,
+  );
+});
 
 test(
   "search for and view the details of a record",
@@ -62,16 +70,26 @@ test(
   async ({ page }) => {
     await page.goto("/catalogue/search/?q=plymouth");
     await expect(page.getByRole("main")).toHaveText(
-      /Showing ([\d,]+) of ([\d,]+)/,
+      /Showing results ([\d,]+)–([\d,]+) of ([\d,]+)/,
     );
-    await expect(page.getByRole("article")).toHaveCount(15);
+    await expect(page.getByRole("article")).toHaveCount(20);
     await page.getByRole("article").first().click();
 
     await expect(page).toHaveURL(new RegExp("/catalogue/id/"));
+    await expect(page).toHaveURL(/\?search=/);
     await expect(page.locator("h1")).not.toBeEmpty();
     await expect(page.getByRole("main")).toHaveText(
       /Catalogue reference: [\w\d\/ ]+/,
     );
     await expect(page.locator("#record-details")).toBeVisible();
+
+    await expect(
+      page.getByRole("link", { name: "Back to search results" }),
+    ).toBeVisible();
+    await page.getByRole("link", { name: "Back to search results" }).click();
+    await page.goto("/catalogue/search/?q=plymouth");
+    await expect(page.getByLabel("Catalogue search results")).toHaveValue(
+      "plymouth",
+    );
   },
 );
