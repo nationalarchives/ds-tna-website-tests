c4647320272d938b96529b088746d4a49fe6ec7f
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-13 17:09:02 +0000

    Refactor sitemap tests to assert content type in response headers

--- tests/browser-independent/desktop-only/sitemaps.spec.ts
+++ tests/browser-independent/desktop-only/sitemaps.spec.ts
@@ -5,28 +5,28 @@ test(
   { tag: ["@wip", "@smoke"] },
   async ({ page }) => {
     const response = await page.goto("/sitemaps/");
-    // const contentType = await response?.headerValue("content-type");
-    // expect(contentType).toEqual("application/xml; charset=utf-8");
+    const contentType = await response?.headerValue("content-type");
+    expect(contentType).toEqual("application/xml; charset=utf-8");
     const status = await response?.status();
     expect(status).toEqual(200);
   },
 );
 
 test("main sitemap", { tag: ["@wip", "@smoke"] }, async ({ page }) => {
-  // page.on("response", async (response) => {
-  //   const contentType = await response.headerValue("content-type");
-  //   expect(contentType).toEqual("application/xml; charset=utf-8");
-  // });
+  page.on("response", async (response) => {
+    const contentType = await response.headerValue("content-type");
+    expect(contentType).toEqual("application/xml; charset=utf-8");
+  });
   const response = await page.goto("/sitemap.xml");
   const status = await response?.status();
   expect(status).toEqual(200);
 });
 
 test("static pages sitemap", { tag: ["@wip", "@smoke"] }, async ({ page }) => {
-  // page.on("response", async (response) => {
-  //   const contentType = await response.headerValue("content-type");
-  //   expect(contentType).toEqual("application/xml; charset=utf-8");
-  // });
+  page.on("response", async (response) => {
+    const contentType = await response.headerValue("content-type");
+    expect(contentType).toEqual("application/xml; charset=utf-8");
+  });
   const response = await page.goto("/sitemaps/sitemap_1.xml");
   const status = await response?.status();
   expect(status).toEqual(200);
@@ -36,10 +36,10 @@ test(
   "first dynamic pages sitemap",
   { tag: ["@wip", "@smoke"] },
   async ({ page }) => {
-    // page.on("response", async (response) => {
-    //   const contentType = await response.headerValue("content-type");
-    //   expect(contentType).toEqual("application/xml; charset=utf-8");
-    // });
+    page.on("response", async (response) => {
+      const contentType = await response.headerValue("content-type");
+      expect(contentType).toEqual("application/xml; charset=utf-8");
+    });
     const response = await page.goto("/sitemaps/sitemap_2.xml");
     const status = await response?.status();
     expect(status).toEqual(200);
