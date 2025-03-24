518bb73f0cd97f48f1119fb038c290a0afe1d324
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-24 13:18:06 +0000

    Update feed tests to use .xml extension for URLs

--- tests/browser-independent/desktop-only/blog/feeds.spec.ts
+++ tests/browser-independent/desktop-only/blog/feeds.spec.ts
@@ -6,7 +6,7 @@ test("feeds listing page", { tag: ["@wip"] }, async ({ page }) => {
 });
 
 test("all feed - rss", { tag: ["@wip", "@smoke"] }, async ({ page }) => {
-  const response = await page.goto("/blogs/feeds/all/");
+  const response = await page.goto("/blogs/feeds/all.xml");
   const status = await response?.status();
   expect(status).toEqual(200);
   const contentType = await response?.headerValue("content-type");
@@ -14,7 +14,7 @@ test("all feed - rss", { tag: ["@wip", "@smoke"] }, async ({ page }) => {
 });
 
 test("all feed - atom", { tag: "@wip" }, async ({ page }) => {
-  const response = await page.goto("/blogs/feeds/all/?format=atom");
+  const response = await page.goto("/blogs/feeds/all.xml?format=atom");
   const status = await response?.status();
   expect(status).toEqual(200);
   const contentType = await response?.headerValue("content-type");
@@ -22,7 +22,7 @@ test("all feed - atom", { tag: "@wip" }, async ({ page }) => {
 });
 
 test("explore the collection feed - rss", { tag: "@wip" }, async ({ page }) => {
-  const response = await page.goto("/blogs/feeds/427/");
+  const response = await page.goto("/blogs/feeds/427.xml");
   const status = await response?.status();
   expect(status).toEqual(200);
   const contentType = await response?.headerValue("content-type");
@@ -33,7 +33,7 @@ test(
   "explore the collection feed - atom",
   { tag: "@wip" },
   async ({ page }) => {
-    const response = await page.goto("/blogs/feeds/427/?format=atom");
+    const response = await page.goto("/blogs/feeds/427.xml?format=atom");
     const status = await response?.status();
     expect(status).toEqual(200);
     const contentType = await response?.headerValue("content-type");
@@ -42,7 +42,7 @@ test(
 );
 
 test("non-existant feed", { tag: "@wip" }, async ({ page }) => {
-  const response = await page.goto("/blogs/feeds/0/");
+  const response = await page.goto("/blogs/feeds/0.xml");
   const status = await response?.status();
   expect(status).toEqual(404);
   const contentType = await response?.headerValue("content-type");
