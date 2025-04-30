806148d25fcbc40c59dfe91b4dc457c3078e58a3
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-04-30 11:45:46 +0100

    Add localhost entry to cookie domain mapping

--- tests/lib/domains.ts
+++ tests/lib/domains.ts
@@ -6,15 +6,14 @@ const getCookieDomainFromBaseUrl: (baseURL: string | undefined) => string = (
     "https://staging-www.nationalarchives.gov.uk": ".nationalarchives.gov.uk",
     "https://dev-www.nationalarchives.gov.uk": ".nationalarchives.gov.uk",
     "http://localhost:65535": "localhost:65535",
+    "https://localhost": "localhost",
     // TODO: Remove dblclk.dev once moved fully to AWS
     "https://tna.dblclk.dev": ".dblclk.dev",
     "https://develop.tna.dblclk.dev": ".dblclk.dev",
   };
-
   if (cookieDomains.hasOwnProperty(baseURL)) {
     return cookieDomains[baseURL];
   }
-
   return `.${baseURL.replace(/^https?:\/\/(www.)?/, "")}`;
 };
 
