6f51d8cc731efda92bba8bed333fe7a6f9eb4db3
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-05-30 10:53:26 +0100

    Enhance sitemap tests and add WordPress sitemap checks

--- tests/browser-independent/sitemaps.spec.ts
+++ tests/browser-independent/sitemaps.spec.ts
@@ -12,27 +12,37 @@ test(
   },
 );
 
-test("main sitemap", { tag: ["@wip", "@smoke"] }, async ({ page }) => {
+test("main sitemap", { tag: ["@wip", "@smoke"] }, async ({ page, baseURL }) => {
   const response = await page.goto("/sitemap.xml");
   const status = await response?.status();
   expect(status).toEqual(200);
   const contentType = await response?.headerValue("content-type");
   expect(contentType).toEqual("application/xml; charset=utf-8");
+  const xmlContent = await response?.text();
+  expect(xmlContent).toContain(`<loc>${baseURL}/sitemaps/sitemap_1.xml</loc>`);
+  expect(xmlContent).toContain(
+    "<loc>https://www.nationalarchives.gov.uk/sitemap_index.xml</loc>",
+  );
 });
 
 test(
   "first dynamic pages sitemap",
   { tag: ["@wip", "@smoke"] },
-  async ({ page }) => {
+  async ({ page, baseURL }) => {
     const response = await page.goto("/sitemaps/sitemap_1.xml");
     const status = await response?.status();
     expect(status).toEqual(200);
     const contentType = await response?.headerValue("content-type");
     expect(contentType).toEqual("application/xml; charset=utf-8");
+    const xmlContent = await response?.text();
+    expect(xmlContent).toContain(`<loc>${baseURL}/</loc>`);
+    expect(xmlContent).toContain(
+      `<loc>${baseURL}/explore-the-collection/</loc>`,
+    );
   },
 );
 
-test("non-existant sitemap", { tag: "@wip" }, async ({ page }) => {
+test("non-existant Wagtail sitemap", { tag: "@wip" }, async ({ page }) => {
   const response = await page.goto("/sitemaps/sitemap_99999.xml");
   const status = await response?.status();
   expect(status).toEqual(404);
@@ -41,4 +51,20 @@ test("non-existant sitemap", { tag: "@wip" }, async ({ page }) => {
   await expect(
     page.getByRole("main").getByRole("heading", { name: "Page not found" }),
   ).toBeVisible();
+});
+
+test("WordPress sitemap index", { tag: "@wip" }, async ({ page }) => {
+  const response = await page.goto("/sitemap_index.xml");
+  const status = await response?.status();
+  expect(status).toEqual(200);
+  const contentType = await response?.headerValue("content-type");
+  expect(contentType).toEqual("text/xml; charset=UTF-8");
+});
+
+test("WordPress page sitemap 1", { tag: "@wip" }, async ({ page }) => {
+  const response = await page.goto("/page-sitemap.xml");
+  const status = await response?.status();
+  expect(status).toEqual(200);
+  const contentType = await response?.headerValue("content-type");
+  expect(contentType).toEqual("text/xml; charset=UTF-8");
 });
