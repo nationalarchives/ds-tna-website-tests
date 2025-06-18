38daa94d7c4b905351ee586fbc7e854abb798409
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-06-18 17:19:45 +0100

    Add initial test for blogs landing page

---
+++ tests/blogs.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+import acceptAllCookies from "./lib/accept-all-cookies.ts";
+
+acceptAllCookies();
+
+test("blogs landing page", async ({ page }) => {
+  await page.goto("/blogs/");
+  await expect(page.locator("h1")).toHaveText(/Blogs at The National Archives/);
+});
