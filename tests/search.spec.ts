51f6a23a0a486be00952c98f4eafae998852c1ce
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-07-30 13:39:37 +0100

    Update search test

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -16,9 +16,9 @@ test("search for a page", { tag: ["@wip"] }, async ({ page }) => {
     .fill("explore the collection");
   await page.getByRole("button", { name: "Search" }).click();
 
-  await expect(page).toHaveURL(/\/search\/\?q=explore\+the\+collection/);
+  await expect(page).toHaveURL(/q=explore\+the\+collection/);
   await expect(page.getByRole("main")).toHaveText(
-    /Showing ([\d,]+)–([\d,]+) of ([\d,]+) results for "explore the collection"/,
+    /Showing ([\d,]+)–([\d,]+) of ([\d,]+) results for\s+"(explore|the|collection)",\s+"(explore|the|collection)"\s+or\s+"(explore|the|collection)"/,
   );
   await expect((await page.getByRole("article").all()).length).toBeGreaterThan(
     0,
@@ -26,7 +26,19 @@ test("search for a page", { tag: ["@wip"] }, async ({ page }) => {
 
   await page.getByRole("link", { name: "Next page" }).click();
   await expect(page.getByRole("main")).toHaveText(
-    /Showing ([\d,]+)–([\d,]+) of ([\d,]+) results for "explore the collection"/,
+    /Showing ([\d,]+)–([\d,]+) of ([\d,]+) results for\s+"(explore|the|collection)",\s+"(explore|the|collection)"\s+or\s+"(explore|the|collection)"/,
+  );
+  await expect((await page.getByRole("article").all()).length).toBeGreaterThan(
+    0,
+  );
+  await page
+    .getByLabel("Search The National Archives website")
+    .fill('"explore the collection"');
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await expect(page).toHaveURL(/q=%22explore\+the\+collection%22/);
+  await expect(page.getByRole("main")).toHaveText(
+    /Showing ([\d,]+)–([\d,]+) of ([\d,]+) results for\s+"explore the collection"/,
   );
   await expect((await page.getByRole("article").all()).length).toBeGreaterThan(
     0,
