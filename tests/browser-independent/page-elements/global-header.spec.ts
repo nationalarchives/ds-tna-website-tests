5d4860dba0629be9e3bdc5a8b038d0f87c0420c5
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-08-21 10:14:50 +0100

    Add WIP tags to global header tests for better tracking

--- tests/browser-independent/page-elements/global-header.spec.ts
+++ tests/browser-independent/page-elements/global-header.spec.ts
@@ -1,37 +1,39 @@
 import { test, expect } from "@playwright/test";
 
-test("global header has the correct markup with HTML only", async ({
-  page,
-  baseURL,
-}) => {
-  await page.route("**/*", (route) => {
-    return ["script", "stylesheet"].includes(route.request().resourceType())
-      ? route.abort()
-      : route.continue();
-  });
-  await page.goto("/explore-the-collection/");
-  const headerMain = await page.locator(".tna-global-header__main");
-  const domainRegEx = new RegExp(baseURL || "", "g");
-  const headerMainHtml = (await headerMain.innerHTML()).replace(
-    domainRegEx,
-    "https://www.nationalarchives.gov.uk",
-  );
-  await expect(headerMainHtml).toMatchSnapshot("global-header-html.txt");
-});
+test(
+  "global header has the correct markup with HTML only",
+  { tag: ["@wip"] },
+  async ({ page, baseURL }) => {
+    await page.route("**/*", (route) => {
+      return ["script", "stylesheet"].includes(route.request().resourceType())
+        ? route.abort()
+        : route.continue();
+    });
+    await page.goto("/explore-the-collection/");
+    const headerMain = await page.locator(".tna-global-header__main");
+    const domainRegEx = new RegExp(baseURL || "", "g");
+    const headerMainHtml = (await headerMain.innerHTML()).replace(
+      domainRegEx,
+      "https://www.nationalarchives.gov.uk",
+    );
+    await expect(headerMainHtml).toMatchSnapshot("global-header-html.txt");
+  },
+);
 
-test("global header has the correct markup after JS is added", async ({
-  page,
-  baseURL,
-}) => {
-  await page.goto("/explore-the-collection/");
-  const headerMain = await page.locator(".tna-global-header__main");
-  const domainRegEx = new RegExp(baseURL || "", "g");
-  const headerMainHtml = (await headerMain.innerHTML()).replace(
-    domainRegEx,
-    "https://www.nationalarchives.gov.uk",
-  );
-  await expect(headerMainHtml).toMatchSnapshot(
-    "global-header-html-with-js.txt",
-  );
-  // await expect(headerMain).toHaveScreenshot("global-header-html+js.png");
-});
+test(
+  "global header has the correct markup after JS is added",
+  { tag: ["@wip"] },
+  async ({ page, baseURL }) => {
+    await page.goto("/explore-the-collection/");
+    const headerMain = await page.locator(".tna-global-header__main");
+    const domainRegEx = new RegExp(baseURL || "", "g");
+    const headerMainHtml = (await headerMain.innerHTML()).replace(
+      domainRegEx,
+      "https://www.nationalarchives.gov.uk",
+    );
+    await expect(headerMainHtml).toMatchSnapshot(
+      "global-header-html-with-js.txt",
+    );
+    // await expect(headerMain).toHaveScreenshot("global-header-html+js.png");
+  },
+);
