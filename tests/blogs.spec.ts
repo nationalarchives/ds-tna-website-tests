edb220b902784a0546f1a9ec5a9a254423aa68a1
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-12-05 14:39:00 +0000

    Refactor blog page tests to check header visibility and non-emptiness

--- tests/blogs.spec.ts
+++ tests/blogs.spec.ts
@@ -5,10 +5,12 @@ acceptAllCookies();
 
 test("blogs landing page", async ({ page }) => {
   await page.goto("/blogs/");
-  await expect(page.locator("h1")).toHaveText(/Blogs at The National Archives/);
+  await expect(page.locator("h1")).not.toBeEmpty();
+  await expect(page.locator("h1")).toBeVisible();
 });
 
 test("feeds listing page", async ({ page }) => {
   await page.goto("/blogs/feeds/");
-  await expect(page.locator("h1")).toHaveText(/Blog feeds/);
+  await expect(page.locator("h1")).not.toBeEmpty();
+  await expect(page.locator("h1")).toBeVisible();
 });
