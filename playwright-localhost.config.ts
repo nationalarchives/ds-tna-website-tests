e3a0f908d4c1ba7ec59c97edaef1594345dd435d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-02-02 11:11:48 +0000

    Set maxFailures to 1 in Playwright configuration

--- playwright-localhost.config.ts
+++ playwright-localhost.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   ...config,
   testDir: "./tests",
+  maxFailures: 1,
   use: {
     ...config.use,
     baseURL: process.env.TEST_DOMAIN || "https://localhost",
