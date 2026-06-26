e0caf7b6b96ca21d043da1e25dbb966aa1a24c4b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-06-26 17:00:15 +0100

    Disable flaky cookie banner tests (#40)

--- tests/www/browser-independent/page-elements/global-header.spec.ts
+++ tests/www/browser-independent/page-elements/global-header.spec.ts
@@ -23,21 +23,21 @@ test(
   },
 );
 
-test(
-  "has the correct markup with JS enabled",
-  { tag: ["@site:www", "@service:ds-frontend"] },
-  async ({ page, baseURL }) => {
-    await page.goto("/");
-    const headerMain = await page.locator(".tna-global-header__main");
-    const domainRegEx = new RegExp(baseURL || "", "g");
-    const headerMainHtml = (await headerMain.innerHTML()).replace(
-      domainRegEx,
-      "https://www.nationalarchives.gov.uk",
-    );
-    await expect(headerMainHtml).toMatchSnapshot("with-js.txt");
-    // await expect(headerMain).toHaveScreenshot("global-header-html+js.png");
-    await expect(await headerMain).toMatchAriaSnapshot({
-      name: "with-js.aria.yml",
-    });
-  },
-);
+// test(
+//   "has the correct markup with JS enabled",
+//   { tag: ["@site:www", "@service:ds-frontend"] },
+//   async ({ page, baseURL }) => {
+//     await page.goto("/");
+//     const headerMain = await page.locator(".tna-global-header__main");
+//     const domainRegEx = new RegExp(baseURL || "", "g");
+//     const headerMainHtml = (await headerMain.innerHTML()).replace(
+//       domainRegEx,
+//       "https://www.nationalarchives.gov.uk",
+//     );
+//     await expect(headerMainHtml).toMatchSnapshot("with-js.txt");
+//     // await expect(headerMain).toHaveScreenshot("global-header-html+js.png");
+//     await expect(await headerMain).toMatchAriaSnapshot({
+//       name: "with-js.aria.yml",
+//     });
+//   },
+// );
