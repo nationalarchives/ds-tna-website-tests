905d4fcf0ed0fdaf5bb7c6656310d380fdad5c6b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-06 12:16:31 +0100

    Update cookie tests, fix header footer and cookie banner tests (#47)

--- tests/www/browser-independent/page-elements/global-header.spec.ts
+++ tests/www/browser-independent/page-elements/global-header.spec.ts
@@ -1,43 +1,38 @@
 import { test, expect } from "@playwright/test";
 
 test(
-  "has the correct HTML with no JS",
+  "has the correct accessibility tree",
   { tag: ["@site:www", "@service:ds-frontend"] },
-  async ({ page, baseURL }) => {
-    await page.route("**/*", (route) => {
-      return ["script", "stylesheet"].includes(route.request().resourceType())
-        ? route.abort()
-        : route.continue();
-    });
+  async ({ page }) => {
     await page.goto("/");
-    const headerMain = await page.locator(".tna-global-header__main");
-    const domainRegEx = new RegExp(baseURL || "", "g");
-    const headerMainHtml = (await headerMain.innerHTML()).replace(
-      domainRegEx,
-      "https://www.nationalarchives.gov.uk",
-    );
-    await expect(headerMainHtml).toMatchSnapshot("no-js.txt");
-    await expect(await headerMain).toMatchAriaSnapshot({
-      name: "no-js.aria.yml",
+    const header = await page.locator(".tna-global-header");
+    await expect(header).toMatchAriaSnapshot({
+      name: "header.aria.yml",
     });
   },
 );
 
 // test(
-//   "has the correct markup with JS enabled",
+//   "has the correct screenshot",
 //   { tag: ["@site:www", "@service:ds-frontend"] },
-//   async ({ page, baseURL }) => {
+//   async ({ page }) => {
 //     await page.goto("/");
-//     const headerMain = await page.locator(".tna-global-header__main");
-//     const domainRegEx = new RegExp(baseURL || "", "g");
-//     const headerMainHtml = (await headerMain.innerHTML()).replace(
-//       domainRegEx,
-//       "https://www.nationalarchives.gov.uk",
-//     );
-//     await expect(headerMainHtml).toMatchSnapshot("with-js.txt");
-//     // await expect(headerMain).toHaveScreenshot("global-header-html+js.png");
-//     await expect(await headerMain).toMatchAriaSnapshot({
-//       name: "with-js.aria.yml",
+//     const header = await page.locator(".tna-global-header");
+//     await expect(header).toHaveScreenshot("global-header.png");
+//   },
+// );
+
+// test(
+//   "has the correct screenshot with no JS or CSS",
+//   { tag: ["@site:www", "@service:ds-frontend"] },
+//   async ({ page }) => {
+//     await page.route("**/*", (route) => {
+//       return ["script", "stylesheet"].includes(route.request().resourceType())
+//         ? route.abort()
+//         : route.continue();
 //     });
+//     await page.goto("/");
+//     const header = await page.locator(".tna-global-header");
+//     await expect(header).toHaveScreenshot("global-header-no-js.png");
 //   },
 // );
